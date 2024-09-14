fd4901e63d3af9b4b32650f37864057fc6f6820e
abdosql <abdosql@users.noreply.github.com>
2024-09-15 01:52:31 +0200

    Implement exception handling for LLM integration- Add LlmIntegrationExceptionEvent for custom event handling- Create LlmIntegrationExceptionListener for centralized exception logging

--- src/Event/LlmIntegrationExceptionEvent.php
+++ src/Event/LlmIntegrationExceptionEvent.php
@@ -6,6 +6,31 @@
 
 namespace Saqqal\LlmIntegrationBundle\Event;
 
-class LlmIntegrationExceptionEvent
+use Saqqal\LlmIntegrationBundle\Exception\LlmIntegrationException;
+use Symfony\Contracts\EventDispatcher\Event;
+
+/**
+ * This class represents an event that is dispatched when an LlmIntegrationException occurs.
+ * It provides a way to handle and react to such exceptions in a centralized manner.
+ */
+class LlmIntegrationExceptionEvent extends Event
 {
+    /**
+     * Constructs a new LlmIntegrationExceptionEvent instance.
+     *
+     * @param LlmIntegrationException $exception The exception that occurred.
+     */
+    public function __construct(private readonly LlmIntegrationException $exception)
+    {
+    }
+
+    /**
+     * Returns the exception that occurred.
+     *
+     * @return LlmIntegrationException The exception that occurred.
+     */
+    public function getException(): LlmIntegrationException
+    {
+        return $this->exception;
+    }
 }
--- src/EventListener/LlmIntegrationExceptionListener.php
+++ src/EventListener/LlmIntegrationExceptionListener.php
@@ -6,6 +6,45 @@
 
 namespace Saqqal\LlmIntegrationBundle\EventListener;
 
+use Psr\Log\LoggerInterface;
+use Saqqal\LlmIntegrationBundle\Event\LlmIntegrationExceptionEvent;
+
+/**
+ * This class listens for LlmIntegrationException events and logs them.
+ *
+ * @author Saqqal Abdelaziz <seqqal.abdelaziz@gmail.com>
+ * @Linkedin https://www.linkedin.com/abdelaziz-saqqal
+ */
 class LlmIntegrationExceptionListener
 {
+    private LoggerInterface $logger;
+
+    /**
+     * Constructor.
+     *
+     * @param LoggerInterface $logger The logger service to use for logging.
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    /**
+     * Handles LlmIntegrationException events.
+     *
+     * Logs the exception message and additional information using the logger service.
+     *
+     * @param LlmIntegrationExceptionEvent $event The event containing the exception.
+     *
+     * @return void
+     */
+    public function onException(LlmIntegrationExceptionEvent $event): void
+    {
+        $exception = $event->getException();
+
+        $this->logger->error('LLM Integration Exception: ' . $exception->getMessage(), [
+            'exception' => $exception,
+            'class' => get_class($exception),
+        ]);
+    }
 }
